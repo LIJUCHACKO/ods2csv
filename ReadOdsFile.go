8c8f651de131af852e65739ea8287e6ca6139cda
liju <liju.g.chacko@gmail.com>
2021-05-10 15:06:57 +0530

    implemented goroutine to read sheets

--- ReadOdsFile.go
+++ ReadOdsFile.go
@@ -20,7 +20,7 @@ package ods
 
 import (
 	"archive/zip"
-	"fmt"
+	//	"fmt"
 	"io/ioutil"
 	"strconv"
 	"strings"
@@ -49,6 +49,10 @@ type Sheet struct {
 type Odsfile struct {
 	Sheets []Sheet
 }
+type Result struct {
+	Sheeti Sheet
+	err    error
+}
 
 func ReplaceHTMLSpecialEntities(input string) string {
 	output := strings.Replace(input, "&amp;", "&", -1)
@@ -64,7 +68,135 @@ func ReplaceHTMLSpecialEntities(input string) string {
 
 	return output
 }
+func ReadSheet(DB *xmlDB.Database, spreadsheet int) (Sheet, error) {
+	tablename := xmlDB.GetNodeAttribute(DB, spreadsheet, "table:name")
+	//fmt.Printf("\nStarted %s\n", tablename)
+	csvrows := []Row{}
+	rows, _ := xmlDB.GetNode(DB, spreadsheet, "table:table-row")
+	blankrows := 0
 
+	for _, row := range rows {
+		rowisblank := true
+		row_repetitionTXT := xmlDB.GetNodeAttribute(DB, row, "table:number-rows-repeated")
+		row_repetition := 1
+		if len(strings.TrimSpace(row_repetitionTXT)) > 0 {
+			value, err := strconv.Atoi(row_repetitionTXT)
+			if err != nil {
+				return Sheet{tablename, csvrows}, err
+			}
+			row_repetition = value
+		}
+		csvcells := []Cell{}
+		Cells, _ := xmlDB.GetNode(DB, row, "table:table-cell")
+		blankcells := 0
+		for _, cell := range Cells {
+			cell_repetitionTXT := xmlDB.GetNodeAttribute(DB, cell, "table:number-columns-repeated")
+			cell_repetition := 1
+			if len(strings.TrimSpace(cell_repetitionTXT)) > 0 {
+				value, err := strconv.Atoi(cell_repetitionTXT)
+				if err != nil {
+					return Sheet{tablename, csvrows}, err
+				}
+				cell_repetition = value
+			}
+			celltype := xmlDB.GetNodeAttribute(DB, cell, "office:value-type")
+			celldatevalue := xmlDB.GetNodeAttribute(DB, cell, "office:date-value")
+			cellvalue := xmlDB.GetNodeAttribute(DB, cell, "office:value")
+			cellformula := xmlDB.GetNodeAttribute(DB, cell, "table:formula")
+
+			Cell_paras, _ := xmlDB.GetNode(DB, cell, "text:p")
+			celltext := ""
+			for _, Cell_para := range Cell_paras {
+				childnodes := xmlDB.ChildNodes(DB, Cell_para)
+				if len(childnodes) > 0 {
+					for _, child := range childnodes {
+						nodeName := xmlDB.GetNodeName(DB, child)
+						if nodeName == "text:s" {
+							cell_space := 1
+							cell_spaceTXT := xmlDB.GetNodeAttribute(DB, child, "text:c")
+							if len(strings.TrimSpace(cell_spaceTXT)) > 0 {
+								value, err := strconv.Atoi(cell_spaceTXT)
+								if err != nil {
+									return Sheet{tablename, csvrows}, err
+								}
+								cell_space = value
+							}
+							for {
+								if cell_space == 0 {
+									break
+								}
+								celltext = celltext + " "
+								cell_space--
+							}
+						}
+						celltext = celltext + xmlDB.GetNodeValue(DB, child)
+					}
+				} else {
+					if len(celltext) > 0 {
+						celltext = celltext + "\n" + xmlDB.GetNodeValue(DB, Cell_para)
+					} else {
+						celltext = xmlDB.GetNodeValue(DB, Cell_para)
+					}
+
+				}
+			}
+
+			celltext = ReplaceHTMLSpecialEntities(celltext)
+			cellvalue = ReplaceHTMLSpecialEntities(cellvalue)
+			if len(celltext) == 0 {
+				blankcells = blankcells + cell_repetition
+			} else {
+				//insert blankcells before newcells
+				for {
+					if blankcells == 0 {
+						break
+					}
+					csvcells = append(csvcells, Cell{"", "", "", "", ""})
+					blankcells--
+
+				}
+				for {
+					if cell_repetition == 0 {
+						break
+					}
+					csvcells = append(csvcells, Cell{celltype, cellvalue, celldatevalue, cellformula, celltext})
+					cell_repetition--
+					rowisblank = false
+				}
+			}
+
+		}
+		if rowisblank {
+			blankrows = blankrows + row_repetition
+		} else {
+			//insert blankrows before newrows
+			for {
+				if blankrows == 0 {
+					break
+				}
+				csvrows = append(csvrows, Row{[]Cell{}})
+				blankrows--
+			}
+			for {
+				if row_repetition == 0 {
+					break
+				}
+				csvrows = append(csvrows, Row{csvcells})
+				row_repetition--
+			}
+
+		}
+
+	}
+	return Sheet{tablename, csvrows}, nil
+}
+func ReadSheetThread(DB *xmlDB.Database, spreadsheet int, chres chan Result) {
+	res := new(Result)
+	Sheeti, err := ReadSheet(DB, spreadsheet)
+	res.Sheeti = Sheeti
+	res.err = err
+	chres <- *res
+}
 func ReadODSFile(odsfilename string) (Odsfile, error) {
 	var odsfileContents Odsfile
 	var DB *xmlDB.Database = new(xmlDB.Database)
@@ -90,133 +222,27 @@ func ReadODSFile(odsfilename string) (Odsfile, error) {
 			xmlline := string(xmlfile)
 			xmllines := strings.Split(xmlline, "\n")
 			xmlDB.Load_dbcontent(DB, xmllines)
-			fmt.Printf("\ncontent.xml loaded to xmldb\n")
+			//fmt.Printf("\ncontent.xml loaded to xmldb\n")
 			csvSpreadSheets := []Sheet{}
 			spreadSheets, _ := xmlDB.GetNode(DB, 0, "office:body/office:spreadsheet/table:table")
-
+			chan1 := make(chan Result)
 			for _, spreadsheet := range spreadSheets {
-				tablename := xmlDB.GetNodeAttribute(DB, spreadsheet, "table:name")
-				fmt.Printf("\n%s\n", tablename)
-				csvrows := []Row{}
-				rows, _ := xmlDB.GetNode(DB, spreadsheet, "table:table-row")
-				blankrows := 0
-
-				for index, row := range rows {
-					fmt.Printf("\r%d/%d", index, len(rows))
-					rowisblank := true
-					row_repetitionTXT := xmlDB.GetNodeAttribute(DB, row, "table:number-rows-repeated")
-					row_repetition := 1
-					if len(strings.TrimSpace(row_repetitionTXT)) > 0 {
-						value, err := strconv.Atoi(row_repetitionTXT)
-						if err != nil {
-							return odsfileContents, err
-						}
-						row_repetition = value
-					}
-					csvcells := []Cell{}
-					Cells, _ := xmlDB.GetNode(DB, row, "table:table-cell")
-					blankcells := 0
-					for _, cell := range Cells {
-						cell_repetitionTXT := xmlDB.GetNodeAttribute(DB, cell, "table:number-columns-repeated")
-						cell_repetition := 1
-						if len(strings.TrimSpace(cell_repetitionTXT)) > 0 {
-							value, err := strconv.Atoi(cell_repetitionTXT)
-							if err != nil {
-								return odsfileContents, err
-							}
-							cell_repetition = value
-						}
-						celltype := xmlDB.GetNodeAttribute(DB, cell, "office:value-type")
-						celldatevalue := xmlDB.GetNodeAttribute(DB, cell, "office:date-value")
-						cellvalue := xmlDB.GetNodeAttribute(DB, cell, "office:value")
-						cellformula := xmlDB.GetNodeAttribute(DB, cell, "table:formula")
-
-						Cell_paras, _ := xmlDB.GetNode(DB, cell, "text:p")
-						celltext := ""
-						for _, Cell_para := range Cell_paras {
-							childnodes := xmlDB.ChildNodes(DB, Cell_para)
-							if len(childnodes) > 0 {
-								for _, child := range childnodes {
-									nodeName := xmlDB.GetNodeName(DB, child)
-									if nodeName == "text:s" {
-										cell_space := 1
-										cell_spaceTXT := xmlDB.GetNodeAttribute(DB, child, "text:c")
-										if len(strings.TrimSpace(cell_spaceTXT)) > 0 {
-											value, err := strconv.Atoi(cell_spaceTXT)
-											if err != nil {
-												return odsfileContents, err
-											}
-											cell_space = value
-										}
-										for {
-											if cell_space == 0 {
-												break
-											}
-											celltext = celltext + " "
-											cell_space--
-										}
-									}
-									celltext = celltext + xmlDB.GetNodeValue(DB, child)
-								}
-							} else {
-								if len(celltext) > 0 {
-									celltext = celltext + "\n" + xmlDB.GetNodeValue(DB, Cell_para)
-								} else {
-									celltext = xmlDB.GetNodeValue(DB, Cell_para)
-								}
-
-							}
-						}
-
-						celltext = ReplaceHTMLSpecialEntities(celltext)
-						cellvalue = ReplaceHTMLSpecialEntities(cellvalue)
-						if len(celltext) == 0 {
-							blankcells = blankcells + cell_repetition
-						} else {
-							//insert blankcells before newcells
-							for {
-								if blankcells == 0 {
-									break
-								}
-								csvcells = append(csvcells, Cell{"", "", "", "", ""})
-								blankcells--
-
-							}
-							for {
-								if cell_repetition == 0 {
-									break
-								}
-								csvcells = append(csvcells, Cell{celltype, cellvalue, celldatevalue, cellformula, celltext})
-								cell_repetition--
-								rowisblank = false
-							}
-						}
-
-					}
-					if rowisblank {
-						blankrows = blankrows + row_repetition
-					} else {
-						//insert blankrows before newrows
-						for {
-							if blankrows == 0 {
-								break
-							}
-							csvrows = append(csvrows, Row{[]Cell{}})
-							blankrows--
-						}
-						for {
-							if row_repetition == 0 {
-								break
-							}
-							csvrows = append(csvrows, Row{csvcells})
-							row_repetition--
-						}
-
-					}
-
-				}
-				csvSpreadSheets = append(csvSpreadSheets, Sheet{tablename, csvrows})
+				go ReadSheetThread(DB, spreadsheet, chan1)
 			}
+			read := 0
+			for {
+				if read >= len(spreadSheets) {
+					break
+				}
+				result := <-chan1
+				csvSpreadSheets = append(csvSpreadSheets, result.Sheeti)
+				if result.err != nil {
+					err = result.err
+					return odsfileContents, err
+				}
+				read++
+			}
+			close(chan1)
 			odsfileContents.Sheets = csvSpreadSheets
 			return odsfileContents, err
 		}
